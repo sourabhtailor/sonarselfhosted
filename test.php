2518d6b45d5fd4696ec79feccea16a7ee5433ce6
sourabhtailor <125914555+sourabhtailor@users.noreply.github.com>
2025-03-21 15:21:12 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,10 @@
 <?php
 function testFunction() {
     $unusedVar = "This is not used"; // SonarQube should flag this as an issue
+
+testing test code is added
+    now check how many lines will it count
+    check for 21222 lines outoff 3 lines added
+
 }
 ?>
